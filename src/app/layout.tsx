1b55eae29320dc44882675bbd420d7cde4ad0874
Coding in Flow <info@codinginflow.com>
2024-11-08 11:41:17 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - AI Resume Builder",
+    absolute: "AI Resume Builder",
+  },
+  description:
+    "AI Resume Builder is the easiest way to create a professional resume that will help you land your dream job.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
