eab06c3e95c195810b48e658204c52681112fde1
Patrick Carter <xCarter93@gmail.com>
2024-11-30 15:35:04 -0500

    added dark mode support and theme toggle with nav bar

---
+++ src/app/(main)/Navbar.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/assets/logo.png";
+import { UserButton } from "@clerk/nextjs";
+import { CreditCard } from "lucide-react";
+import ThemeToggle from "@/components/ThemeToggle";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
+
+export default function Navbar() {
+  const { theme } = useTheme();
+  return (
+    <header className="shadow-sm">
+      <div className="mx-auto flex max-w-7xl items-center justify-between gap-3 p-3">
+        <Link href="/resumes" className="flex items-center gap-3">
+          <Image
+            src={logo}
+            alt="Logo"
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
+          <span className="text-xl font-bold tracking-tight">
+            AI Resume Builder
+          </span>
+        </Link>
+        <div className="flex items-center gap-3">
+          <ThemeToggle />
+          <UserButton
+            appearance={{
+              baseTheme: theme === "dark" ? dark : undefined,
+              elements: {
+                avatarBox: {
+                  width: 40,
+                  height: 40,
+                },
+              },
+            }}
+          >
+            <UserButton.MenuItems>
+              <UserButton.Link
+                label="Billing"
+                href="/billing"
+                labelIcon={<CreditCard className="size-4" />}
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        </div>
+      </div>
+    </header>
+  );
+}
